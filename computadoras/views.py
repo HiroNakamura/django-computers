18708e61d350b2780f81e44b71759c0687bc1bb0
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-09-18 15:56:59 -0500

    Se agrego comp list y se edito views

--- computadoras/views.py
+++ computadoras/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Departamento
+from .models import Computadora
 
 def dept_list(request):
     deptos = Departamento.objects.all()
     return render(request, 'computadoras/dept_list.html', {'deptos':deptos})
+
+def comp_list(request):
+    comps = Computadora.objects.all()
+    return return render(request, 'computadoras/comp_list.html', {'comps':comps})
