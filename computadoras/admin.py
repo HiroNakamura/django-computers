be2831aaf26f51a89aa928642b640a995e162840
ARIEL <carraro.ariel@gmail.com>
2018-09-29 21:50:55 -0500

    Se actualizo admin.py
    Para gestionar y personalizar

--- computadoras/admin.py
+++ computadoras/admin.py
@@ -3,6 +3,22 @@ from .models import Departamento
 from .models import Equipo
 from .models import Usuario
 
-admin.site.register(Departamento)
-admin.site.register(Equipo)
-admin.site.register(Usuario)
+class AdminDepartamento(admin.ModelAdmin):
+  list_display = []
+  class Meta:
+    model = Departamento
+
+class AdminEquipo(admin.ModelAdmin):
+  list_display = []
+  class Meta:
+    model = Equipo
+    
+class AdminUsuario(admin.ModelAdmin):
+  list_display = []
+  class Meta:
+    model = Usuario
+    
+    
+admin.site.register(Departamento,AdminDepartamento)
+admin.site.register(Equipo,AdminEquipo)
+admin.site.register(Usuario,AdminUsuario)
