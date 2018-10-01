5fb0ae0e48bd867baf571be3e2fd8cd6657241cf
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-10-01 10:33:31 -0500

    Se edito admin.py

--- computadoras/admin.py
+++ computadoras/admin.py
@@ -5,7 +5,7 @@ from .models import Usuario
 
 class AdminDepartamento(admin.ModelAdmin):
   list_display = ["area","responsable"]
-  list_display_links = ["responsable"]
+  #list_display_links = ["responsable"]
   list_filter = ["area"]
   list_editable = ["responsable"]
   search_fields = ["area","responsable"]
@@ -13,19 +13,19 @@ class AdminDepartamento(admin.ModelAdmin):
     model = Departamento
 
 class AdminEquipo(admin.ModelAdmin):
-  list_display = ["bien","arrendado","asignao","ip","departamento"]
-  list_display_links = ["bien"]
+  list_display = ["bien","arrendado","asignado","ip","departamento"]
+  #list_display_links = ["bien"]
   list_filter = ["bien","arrendado","asignado","ip"]
-  list_editable = ["asignado","ip","dns","piso","actualizada","departamento"]
+  list_editable = ["asignado","ip","departamento"]
   search_fields = ["bien","arrendado","asignado","ip","actualizada"]
   class Meta:
     model = Equipo
     
 class AdminUsuario(admin.ModelAdmin):
   list_display = ["usuario","nombre","apellidos","cargo"]
-  list_display_links = ["cargo"]
+  #list_display_links = ["cargo"]
   list_filter = ["usuario","nombre","apellidos","cargo"]
-  list_editable = ["usuario","nombre","apellidos","cargo","password","computadora"]
+  list_editable = ["usuario","nombre","apellidos","cargo"]
   search_fields = ["usuario","nombre","apellidos","cargo","computadora"]
   class Meta:
     model = Usuario
