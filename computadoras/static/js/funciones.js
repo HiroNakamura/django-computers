3fccd7f32c667574d8e04988eae231e3f0dcf87f
HIRONAKAMURA <carraro.ariel@gmail.com>
2018-10-02 12:04:23 -0500

    Mas cambios

---
+++ computadoras/static/js/funciones.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log('%c Javascript activo! ', 'background:#bada55 ; color: #0101DF');
+
+    
+});
---
+++ computers/static/js/funciones.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log('%c Javascript activo! ', 'background: #222; color: #bada55');
+
+    
+});
